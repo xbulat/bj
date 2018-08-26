8986af44126d67e11fb0da1b049643deb296bfd8
Alexandr Bulatov <cruc@enicad.com>
2018-08-26 15:13:10 +0300

    Rename class file : card -> card_deck

---
+++ lib/card_deck.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+class CardDeck
+  attr_reader :deck
+
+  def initialize
+    @deck = []
+    shuffle_deck
+  end
+
+  def give_card
+    deck.pop
+  end
+
+  private
+
+  attr_writer :deck
+
+  def shuffle_deck
+    %w(♠ ♥ ♣ ♦).each do |m|
+      (2..10).each { |s| deck << s.to_s + m }
+      %w(K Q J A).each { |s| deck << s + m }
+    end
+    deck.shuffle!
+  end
+end
